7daf2efd3a28ae1e1c99f9a03710fe64ed6944c8
AgnieszkaWw <agnieszkawie82@onet.pl>
2024-01-24 12:58:18 +0100

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,8 @@
+"use strict";
+const rows = document.querySelectorAll("tr");
+rows.forEach((row)=>{
+    const element = row.children[1].cloneNode(true);
+    row.children[3].after(element);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
